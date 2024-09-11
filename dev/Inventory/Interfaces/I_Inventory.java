1a6e93f31c3aeeb9127821c791ab6d606ef0e7fb
omrihershko <omrihers@post.bgu.ac.il>
2024-09-11 15:28:17 +0300

    fix the interface inventory

--- dev/Inventory/Interfaces/I_Inventory.java
+++ dev/Inventory/Interfaces/I_Inventory.java
@@ -11,28 +11,28 @@ public interface I_Inventory {
 
 
     // Adding an item to the inventory
-    public void addItem(Item item);
+     void addItem(Item item);
 
     // Removing an item from the inventory
-    public void removeItem(Item item);
+     void removeItem(Item item);
 
     // Adding a product to the inventory
-    public void addProduct(Product product);
+     void addProduct(Product product);
 
     // Retrieving a product by name
-    public Product getProductByName(String name);
+     Product getProductByName(String name);
 
     // Retrieving a list of products by status
-    public List<Product> getProductsByStatus(E_Product_Status status);
+     List<Product> getProductsByStatus(E_Product_Status status);
 
     // Retrieving a list of products by category
-    public List<Product> getProductsByCategory(String category);
+     List<Product> getProductsByCategory(String category);
 
     // Retrieving a list of products by sub-category
-    public List<Product> getProductsBySubCategory(String subcategory);
+     List<Product> getProductsBySubCategory(String subcategory);
 
     // Moving an item to a different place in the inventory
-    public void moveItemTo(Item item, E_Item_Place place);
+     void moveItemTo(Item item, E_Item_Place place);
 
     // Override of the `toString()` method to print out the inventory details
     @Override
